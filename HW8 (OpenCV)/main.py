bb6f035afb14cc7361d21fcdf6ad178a79ffffc1
BAD-LIAR <gvozdevnikita1@gmail.com>
2022-12-28 00:03:56 +0300

    hw8

--- HW8 (OpenCV)/main.py
+++ HW8 (OpenCV)/main.py
@@ -6,7 +6,7 @@ while True:
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(image_gray, 1.3, 5)
     for x, y, width, height in faces:
-        cv2.rectangle(image, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=2)
+        cv2.rectangle(image, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=2) 
     cv2.imshow("image", image)
     if cv2.waitKey(1) == ord("q"):
         break
